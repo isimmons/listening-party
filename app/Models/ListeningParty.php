b77958623b3c39b760810d414a5a51aae2e61d9a
Ian <isimmons33@gmail.com>
2024-09-20 04:44:24 -0400

    update migration, add state

--- app/Models/ListeningParty.php
+++ app/Models/ListeningParty.php
@@ -11,6 +11,12 @@ class ListeningParty extends Model
     use HasFactory;
     protected $guarded = ['id'];
 
+    protected $casts = [
+        'is_active' => 'boolean',
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+    ];
+
     public function episode(): BelongsTo
     {
         return $this->belongsTo(Episode::class);
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,9 +1,23 @@
 <?php
 
+use App\Models\ListeningParty;
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    public string $name = '';
+    public datetime $startTime;
+
+    public function createListeningParty()
+    {
+
+    }
+
+    public function with()
+    {
+        return [
+            'listening_parties' => ListeningParty::all(),
+        ];
+    }
 }; ?>
 
 <div>
